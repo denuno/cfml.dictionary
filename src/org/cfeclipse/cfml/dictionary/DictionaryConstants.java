020a3b447a8cdad0bbe0bbc63be61c56a987c385
denny <denny@wee>
2010-02-10 15:23:33 -0700

    initial commit : the most unimaginitive initial commit message possible

---
+++ src/org/cfeclipse/cfml/dictionary/DictionaryConstants.java
@@ -0,0 +1,24 @@
+package org.cfeclipse.cfml.dictionary;
+
+public class DictionaryConstants {
+	
+		/** this is the per project setting for their dictionary selection */
+    public static final String P_CFML_DICTIONARY = "cfmlDictionary";
+    
+    /** the default key to the dictionary to use in this project.
+     * @see DictionaryManager 
+     */
+    public static final String P_CFML_DICTIONARY_DEFAULT = "cf9";
+
+	/** the coldfusion dictionaries folder */
+	public static final String DICTIONARY_DIR = "file://dictionaries/";
+	/** the coldfusion dictionary */
+	public static final String CFDIC = "CF_DICTIONARY";
+	/** the javascript dictionary */
+	public static final String JSDIC = "JS_DICTIONARY";
+	/** the SQL dictionary */
+	public static final String SQLDIC = "SQL_DICTIONARY";
+	/** the (yet to be made) html dictionary */
+	public static final String HTDIC = "HT_DICTIONARY";
+
+}
